eedabe73866f4d3d60ffe9927168751e9754f4e7
George Wu <Georgehzwu@gmail.com>
2019-12-28 21:50:26 -0500

    Added more modifications

--- learned_bloom_filter.py
+++ learned_bloom_filter.py
@@ -28,12 +28,13 @@ def correct_check(x, y):
 
 def generate_data(number = 1000000):
     x = [i for i in range( 999, 2000 )] # positive if it's from 1000 - 2000
+    pickle.dump(x, open("in_range_x.dump", "wb"))
     not_x = [i for i in range(0, number) if not i in x] # Negative otherwise
     some_x = sample(not_x, 1000) # 1000 numbers from outside
+    pickle.dump(some_x, open("out_range_x.dump", "wb"))
 
     x.extend(some_x)
     not_x = [ i for i in range(0, number) if not i in x] # Recalculate the negative ones.
-    pickle.dump(x, open("positive_x.dump", "wb"))
     pickle.dump(not_x, open("negative_x.dump", "wb"))
 
 class Net(nn.Module):
@@ -49,23 +50,26 @@ net = Net()
 print(net)
 print(list(net.parameters()))
 
-# Dataset generation
+# Dataset generation and load
+
 # generate_data(1000000)
-x = pickle.load(open("positive_x.dump", "rb"))
+in_range = pickle.load(open("in_range_x.dump", "rb"))
+out_range = pickle.load(open("out_range_x.dump", "rb"))
+x = in_range + out_range
 not_x = pickle.load(open("negative_x.dump", "rb"))
-#x = [i for i in range(999, 2000)] # Positive if it's from 1000 - 2000
-#
-#not_x = [i for i in range (0, total) if not i in x] # Negative otherwise
-#
-#some_x = sample(not_x, 1000) # 1000 from outside as well.
-#x.extend(some_x)
-#not_x = [i for i in range(0,total) if not i in x]
 
 x_true = np.array(x)
 x_false = np.array(not_x)
 
 number_of_examples = 500
-x_positive = np.random.choice(x_true, number_of_examples)
+
+# Get exactly half of number of examples from outside as well as inside.
+x_positive = []
+x_positive.extend(np.random.choice(in_range, int(number_of_examples / 2)))
+x_positive.extend(np.random.choice(out_range, int(number_of_examples/2)))
+x_positive = np.array(x_positive)
+
+x_positive_size = x_positive.size
 
 # Generate a dataset of all our positive data.
 lst = []
@@ -74,7 +78,7 @@ while index < x_true.size:
     lst.append(conv(x_true[index]))
     index += 1
 x_true_data = np.array(lst)
-y_true = np.array([1] * len(x))
+y_true = np.array([1] * x_true.size)
 
 print(x_true_data)
 print(y_true)
@@ -85,6 +89,8 @@ y_true = torch.from_numpy(y_true).long().to('cpu')
 print(x_true_data.size(0))
 print(y_true.size(0))
 total = TensorDataset(x_true_data, y_true)
+
+# All our positive keys are put into this true_loader
 true_loader = DataLoader(dataset=total, batch_size = 10)
 
 # Generate our training tensors. Convert to binary as well. They start as numpy arrays.
@@ -99,6 +105,10 @@ x_positive = np.array(lst)
 y_positive = np.array([1] * number_of_examples)
 
 x_negative = np.random.choice(x_false, number_of_examples)
+
+# These are the keys to insert.
+
+positive_keys_loader = DataLoader(dataset=TensorDataset(torch.from_numpy(x_positive).long().to('cpu'), torch.from_numpy(y_positive).long().to('cpu')), batch_size = 10)
 
 index = 0
 lst = []
@@ -163,14 +173,8 @@ with torch.no_grad():
         
         predict = torch.gt(yhat.data, 0.5)
         #_, predict = torch.max(yhat.data, 1)
-        print("y_val:")
-        print(y_val)
-        print(yhat.data)
-        print(predict)
         total += 10
         for i in range(10):
-            print("correct check")
-            print(correct_check(predict[i], y_val[i]))
             correct += (correct_check(predict[i], y_val[i]))
 
     print("Hello")
@@ -180,28 +184,77 @@ with torch.no_grad():
 
 print(net.state_dict())
 
+tau = 0.5
+# Find tau
+with torch.no_grad():
+    number = 0
+    total = 0
+    for x_val, y_val in val_loader:
+        x_val = x_val.to('cpu')
+        y_val = y_val.to('cpu')
+
+        net.eval()
+
+        yhat = net(x_val.float())
+
+        for i in range(y_val.size(0)):
+            # If it's not supposed to be a key
+            if y_val[i] == 0:
+                if yhat[i] > tau:
+                    number += 1
+            total += 1
+    print("{} number out of {} total. {}".format( number, total, number/total ))
+
+
 # Find the false negatives
-tau = 0.4
+tau = 0.5
 count = 0 
 lst = []
 
 # In this case, our key set is x_train.
 with torch.no_grad():
-    for x_val, y_val in train_loader:
+    for x_val, y_val in positive_keys_loader:
+        x_val = x_val.to('cpu')
+        y_val = y_val.to('cpu')
+
         net.eval()
+
         yhat = net(x_val.float())
-        predict = torch.gt(yhat.data, 0.5)
         for i in range(y_val.size(0)):
-            if correct_check(predict[i], y_val[i]):
+            if yhat[i] >= tau:
                 continue
             else:
-                lst.append(x_val)
-
+                k = x_val[i].tolist()
+                print(k)
+                lst.append(x_val[i])
+print("The length of lst is {} and the x_positive is {}".format(len(lst), x_positive_size))
+print(len(lst))
 bf = BloomFilter(len(x_val), 0.01)
 for i in lst:
     bf.add(str(x_val))
 
 # To find the FPR, we need to sample from the non-known list. "Held-out set of non-keys"
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for x_val, y_val in val_loader:
+        x_val = x_val.to('cpu')
+        y_val = y_val.to('cpu')
+
+        net.eval()
+
+        yhat = net(x_val.float())
+
+        for i in range(y_val.size(0)):
+            if yhat[i] > tau:
+                # assume I return true
+                if y_val[i] == 1:
+                    correct += 1
+            # If it's not supposed to be a key
+            total += 1
+    print("{} number out of {} total. {}".format( number, total, number/total ))
+
+
 
 
 # TODO: Count the number of missed inputs and add to our bloom filter.
